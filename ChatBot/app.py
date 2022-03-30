0c03fadbaf44824e50c7802f164122e29cfba514
4dibbp90 <4dibbp90@solent.ac.uk>
2022-03-30 13:40:22 +0100

    First commit
     Creation of simple web application

---
+++ ChatBot/app.py
@@ -0,0 +1,9 @@
+# importing packages
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+    return render_template ('index.html')
