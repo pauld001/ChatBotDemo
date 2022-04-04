c5651f134c975e1637e5370ffb7f958da87b43fc
4dibbp90 <4dibbp90@solent.ac.uk>
2022-04-04 11:07:25 +0100

    chatbot
    start of development of chatbot "solent"

--- ChatBot/app.py
+++ ChatBot/app.py
@@ -1,9 +1,16 @@
 # importing packages
 from flask import Flask
 from flask import render_template
+from chatterbot import ChatBot, chatterbot
+
 
 app = Flask(__name__)
 
 @app.route("/index")
 def index():
     return render_template ('index.html')
+    
+solent = ChatBot("solent")
+
+
+
