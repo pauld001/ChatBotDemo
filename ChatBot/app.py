bf2bd31da167124c7628efe8d5959a6c09b521fa
4dibbp90 <4dibbp90@solent.ac.uk>
2022-04-05 16:25:07 +0100

    progress 05/04/2022
    creating simple test bot working on receiving input from html

--- ChatBot/app.py
+++ ChatBot/app.py
@@ -1,16 +1,24 @@
 # importing packages
 from flask import Flask
 from flask import render_template
-from chatterbot import ChatBot, chatterbot
+from chatterbot import ChatBot
+from chatterbot_corpus import corpus
+import time
 
 
-app = Flask(__name__)
+chat = Flask(__name__)
 
-@app.route("/index")
+@chat.route("/chatbot")
 def index():
     return render_template ('index.html')
     
-solent = ChatBot("solent")
+solent = ChatBot("solent",
+storage_adapter='chatterbot.storage.SQLStorageAdapter',
+database_uri='sqlite:///database.sqlite3',
+logic_adapters=['chatterbot.logic.TimeLogicAdapter', 'chatterbot.logic.BestMatch']
+)
 
+bot_input = solent.get_response(input())
+print(bot_input)
 
 
