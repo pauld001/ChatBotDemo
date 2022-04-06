3bd4e1e620fc9f351c41a7971c2a80860e89932a
pauld001 <4dibbp90@solent.ac.uk>
2022-04-06 15:01:20 +0100

    created test chatbot ready for test training

--- ChatBot/app.py
+++ ChatBot/app.py
@@ -1,9 +1,10 @@
 # importing packages
 from flask import Flask
 from flask import render_template
+from flask import request
 from chatterbot import ChatBot
 from chatterbot_corpus import corpus
-import time
+#from chatterbot import TimeLogicAdapter
 
 
 chat = Flask(__name__)
@@ -11,14 +12,19 @@ chat = Flask(__name__)
 @chat.route("/chatbot")
 def index():
     return render_template ('index.html')
-    
+
+    userinput = request.form.get('solentinput')
+    print(userinput)
+    bot_input = solent.get_response(userinput)
+    print(bot_input)
+
 solent = ChatBot("solent",
 storage_adapter='chatterbot.storage.SQLStorageAdapter',
 database_uri='sqlite:///database.sqlite3',
 logic_adapters=['chatterbot.logic.TimeLogicAdapter', 'chatterbot.logic.BestMatch']
 )
 
-bot_input = solent.get_response(input())
-print(bot_input)
+
+
 
 
