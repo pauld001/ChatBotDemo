ec18e3c0d75aeaa1cbf36a702ae688799fc6e0ff
pauld001 <4dibbp90@solent.ac.uk>
2022-04-27 15:48:08 +0100

    changed name from solent to student

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # importing packages
 from asyncore import read
-from email.policy import default
 import sys
 from flask import Flask
 from flask import render_template
@@ -16,14 +15,13 @@ chat = Flask(__name__)
 print('working')
 
 
-solent = ChatBot("solent",
+student = ChatBot("student",
       #heroku uri
 #database_uri='postgres://nkcguvuabtmujv:6c8384842f707c8db4d64d28ce92554260989edc0813870436a5cfddf968983e@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d2t6kvgjuj9m8g',
       #test uri
 database_uri='sqlite:///database.sqlite3',
 storage_adapter="chatterbot.storage.SQLStorageAdapter",
-logic_adapters=['chatterbot.logic.BestMatch']
-)
+logic_adapters=[{'import_path':'chatterbot.logic.BestMatch', 'maximum_similarity_threshold': 0.75, 'default_response': 'i currently do not know the answer to this message! Please try again.'}])
 
 
 
@@ -32,7 +30,7 @@ logic_adapters=['chatterbot.logic.BestMatch']
 
 
 def index():
-    solent_input = "Hello i am student the chat bot! Ask me a question or query and i will help you"
+    student_input = "Hello i am student your assistant! Ask me a question or query and i will help you"
     chatpost = ''
     if request.method == 'POST':
         print("post")
@@ -40,11 +38,10 @@ def index():
         #chatpost = input "solent_input"
         chatpost = request.form['solent_input']
         print(chatpost)
-        print('hi')
-        solent_input = solent.get_response(chatpost)
-        print(solent_input)
+        student_input = student.get_response(chatpost)
+        print(student_input)
         
-    return render_template ('index.html', outmessage=solent_input, usermessage=chatpost), print('/chatbot')
+    return render_template ('index.html', outmessage=student_input, usermessage=chatpost)
 
    
 
@@ -53,11 +50,11 @@ if __name__ == "__main__":
 
 
 #training "solent" chatbot
-trainer = ListTrainer(solent)
+trainer = ListTrainer(student)
 
 #greetings
 trainer.train([
-'Hi',
+'Hi''about',
 'Hello i am student the chat bot. ask me a question or query and i will help you',
 
 ])
@@ -71,8 +68,8 @@ trainer.train([
 
 
 trainer.train([
-'map',
-'here is a map of the campus'
+'campus map',
+'here is a map of the campus: https://www.solent.ac.uk/about/documents/east-park-terrace-campus-map.pdf'
 ])
 
 
@@ -80,10 +77,7 @@ trainer.train([
 'i am looking to book openday',
 'here is the form to book an openday'
 ])
-trainer.train([
-'can you help me',
-'yes i can please state how i can help you'
-])
+
 #find a course
 trainer.train([
 'i am looking for a course',
@@ -97,14 +91,20 @@ trainer.train([
 ])
 
 trainer.train([
-'student union'
+'opendays',
+'here are the upcoming opendays: undergraduate: 28th april 2022, cadetship: 14th may 2022, Postgraduate: 18th may 2022. See more here https://www.solent.ac.uk/open-days'
+
+])
+
+trainer.train([
+'student union',
 'here is a link to the student union'
 
 ])
 
 trainer.train([
 'student hand book',
-'here is a link to the student handbook'
+'here is a link to download and read the student handbook https://students.solent.ac.uk/official-documents/quality-management/student-handbook.pdf'
 
 ])
 
@@ -121,7 +121,7 @@ trainer.train([
 
 trainer.train([
 'email',
-'here is a link to outlook to acess your solent email: https://www.office.com/ having problems contact us at: ...'  
+'here is a link to outlook to acess your solent email click hear to sign in: https://www.office.com/ having problems contact us at: ...'  
 ])
 
 trainer.train([
@@ -130,13 +130,13 @@ trainer.train([
 ])
 
 trainer.train([
-    'news',
-    'here is our latest news and events'
+    'news''events',
+    'here is our latest news and events https://students.solent.ac.uk/events'
 ])
 
 trainer.train([
-    'libary opening times',
-    'there are the current libary opening times:'
+    'library opening times',
+    'Tuesday 19th April from 8.30am - Friday 20th May up to 11.45pm: The Library will be open 24/7 see more here: https://libguides.solent.ac.uk/libraryessentials'
 ])
 
 trainer.train([
@@ -144,8 +144,11 @@ trainer.train([
     'please complete this form. and we will get back to you'
 ])
 
+trainer.train([
+    'contact student union',
+    'please complete this form. and we will get back to you. additionaly you can find information on the Student union website https://www.solentsu.co.uk/'
+])
 
-#contact
 
 trainer.train([
     'contact student hub',
@@ -153,13 +156,8 @@ trainer.train([
 ])
 
 trainer.train([
-    'contact student hub',
-    'please complete this form. and we will get back to you'
-])
-
-trainer.train([
     'contact student registry',
-    '<h2>instructions of use</h2>'
+    'please complete this form, or email student.registry@solent.ac.uk, and we will get back to you'
 ])
 
 
